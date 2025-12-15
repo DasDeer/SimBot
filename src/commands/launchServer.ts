e77342fa5ba737b6109b4a53b5dee847d779c2ac
simonhjortikea <simon.hjort@inter.ikea.com>
2025-12-15 17:07:07 +0100

    fix

--- src/commands/launchServer.ts
+++ src/commands/launchServer.ts
@@ -61,7 +61,7 @@ export const data = new SlashCommandBuilder()
         "cmd.exe",
         [
           "/k", // run command then exit
-          `"C:\\Factorio Server\\bin\\x64\\factorio.exe" --start-server "C:\\Users\\Min dator\\AppData\\Roaming\\Factorio\\saves\\SimBot_Server.zip"`
+          `"C:\\FactorioServer\\bin\\x64\\factorio.exe" --start-server "C:\\Users\\Min dator\\AppData\\Roaming\\Factorio\\saves\\SimBot_Server.zip"`
         ],
         {
           cwd: factorio_location,
